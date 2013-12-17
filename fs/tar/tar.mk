3c1c4d108df138c1c4f5d33f6dcdda29a696b0fe
Chris Moore <chris.moore@makerbot.com>
2013-12-17 18:59:28 -0500

    Store the home directory in /var/home with ownership by daemon for NFS builds.  Also NFS builds need connman to be disabled, and all builds need daemon to own /usr/local.

--- fs/tar/tar.mk
+++ fs/tar/tar.mk
@@ -7,7 +7,12 @@
 TAR_OPTS := $(BR2_TARGET_ROOTFS_TAR_OPTIONS)
 
 define ROOTFS_TAR_CMD
- tar -c$(TAR_OPTS)f $@ -C $(TARGET_DIR) .
+ cp -pr $(TARGET_DIR)/../bw-home $(TARGET_DIR)/var/home;\
+ chown -R 1:1 $(TARGET_DIR)/var/home;\
+ mv $(TARGET_DIR)/etc/init.d/S45connman $(TARGET_DIR)/etc/init.d/X45connman;\
+ tar -c$(TAR_OPTS)f $@ -C $(TARGET_DIR) .;\
+ mv $(TARGET_DIR)/etc/init.d/X45connman $(TARGET_DIR)/etc/init.d/S45connman;\
+ rm -rf $(TARGET_DIR)/var/home
 endef
 
 $(eval $(call ROOTFS_TARGET,tar))
