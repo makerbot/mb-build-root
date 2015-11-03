edd17c7924a6e2c51a0a170eaca57caaefcee354
Chris Moore <chris.moore@makerbot.com>
2015-11-03 07:29:09 -0500

    Update zxing to master of zxing-cpp
    The cpp port of zxing was removed from the official zxing repo and is
    now being maintained separately.  There are no official releases of
    this branch, but it does have a cmake build setup, and comes with a
    simple cli client, whereas the current zxing builds only a static
    library, so nothing actually gets installed to the bot.

--- package/zxing/zxing.mk
+++ package/zxing/zxing.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ZXING_VERSION = 2.0
-ZXING_SITE = http://zxing.googlecode.com/files
+ZXING_VERSION = 5ce91bb
+ZXING_SITE = http://y.soft.makerbot.net/files
 ZXING_SOURCE = ZXing-$(ZXING_VERSION).zip
 ZXING_LICENSE = Apache v2.0
 ZXING_LICENSE_FILES = COPYING
@@ -19,24 +19,4 @@ define ZXING_EXTRACT_CMDS
 	unzip -d $(BUILD_DIR) $(DL_DIR)/$(ZXING_SOURCE)
 endef
 
-define ZXING_BUILD_CMDS
-	$(MAKE) -C $(@D)/cpp/core/src $(TARGET_CONFIGURE_OPTS)
-endef
-
-define ZXING_INSTALL_STAGING_CMDS
-	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(STAGING_DIR) install
-endef
-
-define ZXING_UNINSTALL_STAGING_CMDS
-	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(STAGING_DIR) uninstall
-endef
-
-define ZXING_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(TARGET_DIR) install
-endef
-
-define ZXING_UNINSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(TARGET_DIR) uninstall
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
