e8df3d31614423b645068a8caf21a4cb71e624aa
Bill Canfield <wdc.rith@gmail.com>
2016-02-25 14:57:05 -0500

    BW-3149: Adding target side dfu-util as a package option, and adding it to our defconfig

--- package/dfu-util/dfu-util.mk
+++ package/dfu-util/dfu-util.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-DFU_UTIL_VERSION = 0.8
+DFU_UTIL_VERSION = 0.9
+DFU_UTIL_SOURCE = dfu-util-$(DFU_UTIL_VERSION).tar.gz
 DFU_UTIL_SITE = http://dfu-util.sourceforge.net/releases
 DFU_UTIL_LICENSE = GPLv2+
 DFU_UTIL_LICENSE_FILES = COPYING
 
 HOST_DFU_UTIL_DEPENDENCIES = host-libusb
+DFU_UTIL_DEPENDENCIES = libusb
 
+$(eval $(autotools-package))
 $(eval $(host-autotools-package))
