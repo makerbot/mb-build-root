488c629661e92dcfb544bb7eadbd5e0de3979ee6
Ryan Howard <ryan.howard@makerbot.com>
2016-04-06 16:04:34 -0400

    add codernitydb3 to targets and setuptools host-targets

---
+++ package/host-python3-setuptools/host-python3-setuptools.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# python-setuptools
+#
+################################################################################
+
+HOST_PYTHON3_SETUPTOOLS_VERSION = 20.6.7
+HOST_PYTHON3_SETUPTOOLS_SOURCE  = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
+HOST_PYTHON3_SETUPTOOLS_SITE    = https://pypi.python.org/packages/source/s/setuptools/
+HOST_PYTHON3_SETUPTOOLS_DEPENDENCIES = python3
+
+define HOST_PYTHON3_SETUPTOOLS_BUILD_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python3 setup.py build)
+endef
+
+define HOST_PYTHON_SETUPTOOLS_INSTALL_CMDS
+	(cd $(@D); \
+	$(HOST_DIR)/usr/bin/python3 setup.py install --prefix=$(HOST_DIR)/usr)
+endef
+
+$(eval $(host-generic-package))
---
+++ package/python3-codernitydb3/python3-codernitydb3.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# python-codernitydb3
+#
+################################################################################
+
+PYTHON3_CODERNITYDB3_VERSION = master
+PYTHON3_CODERNITYDB3_SOURCE = andy0130tw-CodernityDB3-a3ecf58.tar.gz
+PYTHON3_CODERNITYDB3_SITE = https://github.com/andy0130tw/CodernityDB3/tarball/$(PYTHON_CODERNITYDB3_VERSION)
+
+PYTHON3_CODERNITYDB3_DEPENDENCIES = python3 host-python3-setuptools
+
+define PYTHON3_CODERNITYDB3_BUILD_CMDS
+    (cd $(@D); $(HOST_DIR)/usr/bin/python3 setup.py build)
+endef
+
+define PYTHON3_CODERNITYDB3_INSTALL_TARGET_CMDS
+    (cd $(@D); $(HOST_DIR)/usr/bin/python3 setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+$(eval $(generic-package))
