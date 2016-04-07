3c52ff9baa3c5d907f16d2cc856dffc6b072eced
Ryan Howard <ryan.howard@makerbot.com>
2016-04-07 14:24:07 -0400

    fix config and makefile bugs

--- package/host-python3-setuptools/host-python3-setuptools.mk
+++ package/host-python3-setuptools/host-python3-setuptools.mk
@@ -1,13 +1,13 @@
 ################################################################################
 #
-# python-setuptools
+# host-python3-setuptools
 #
 ################################################################################
 
 HOST_PYTHON3_SETUPTOOLS_VERSION = 20.6.7
-HOST_PYTHON3_SETUPTOOLS_SOURCE  = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
+HOST_PYTHON3_SETUPTOOLS_SOURCE  = setuptools-$(HOST_PYTHON3_SETUPTOOLS_VERSION).tar.gz
 HOST_PYTHON3_SETUPTOOLS_SITE    = https://pypi.python.org/packages/source/s/setuptools/
-HOST_PYTHON3_SETUPTOOLS_DEPENDENCIES = python3
+HOST_PYTHON3_SETUPTOOLS_DEPENDENCIES = host-python3
 
 define HOST_PYTHON3_SETUPTOOLS_BUILD_CMDS
 	(cd $(@D); $(HOST_DIR)/usr/bin/python3 setup.py build)
@@ -18,4 +18,4 @@ define HOST_PYTHON_SETUPTOOLS_INSTALL_CMDS
 	$(HOST_DIR)/usr/bin/python3 setup.py install --prefix=$(HOST_DIR)/usr)
 endef
 
-$(eval $(host-generic-package))
+$(eval $(generic-package))
