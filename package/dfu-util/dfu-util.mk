6d887b92ceea3372ef27dbe1a49bf9c8b9c30b7a
Seth Foster <seth.foster@makerbot.com>
2016-08-17 17:55:38 -0400

    Teach dfu how to compile for target
    And enable it

--- package/dfu-util/dfu-util.mk
+++ package/dfu-util/dfu-util.mk
@@ -6,8 +6,14 @@
 
 DFU_UTIL_VERSION = 0.8
 DFU_UTIL_SITE = http://dfu-util.sourceforge.net/releases
+DFU_UTIL_SOURCE = dfu-util-$(DFU_UTIL_VERSION).tar.gz
 DFU_UTIL_LICENSE = GPLv2+
 DFU_UTIL_LICENSE_FILES = COPYING
+
+DFU_UTIL_INSTALL_STAGING = YES
+DFU_UTIL_INSTALL_TARGET = YES
+DFU_UTIL_DEPENDENCIES = libusb
+$(eval $(autotools-package))
 
 HOST_DFU_UTIL_DEPENDENCIES = host-libusb
 
